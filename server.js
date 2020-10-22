9b8cdfe28d356e1cc2239493611907407efa5ea7
Aasish Rijal <rjlaasish@gmail.com>
2020-10-22 17:33:28 +0545

    port configured

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const connectDB = require("./config/db");
 const PORT = process.env.PORT || 3000;
 // const ejs=require('ejs');
 const path = require("path");
 
 // template engine
+app.use(express.static("public"));
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "ejs");
-app.use(express.static("public/"));
 
 // mongodb connecton
+const connectDB = require("./config/db");
 connectDB();
 
 
@@ -19,6 +19,6 @@ app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
 app.use("/file/download", require("./routes/download"));
 
-app.listen(PORT, () => {
-  console.log("Server Started Sucessfully!");
-});
+app.listen(PORT,
+  console.log("Server Started Sucessfully! "+PORT)
+);
