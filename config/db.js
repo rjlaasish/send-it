81f5c5483f90a61a06d5848de3f150a746f1046c
Aasish Rijal <rjlaasish@gmail.com>
2020-10-22 17:52:07 +0545

    Yes it may be final commit!

--- config/db.js
+++ config/db.js
@@ -6,11 +6,11 @@ const connectDB=()=>{
     mongoose.connect(process.env.MONGO_CONNECTION_URL,{ useUnifiedTopology: true, useNewUrlParser: true});
     const connection=mongoose.connection;
 
-    connection.once('open',()=>{
-        console.log('Database conected!');
-    }).catch(err=>{
-        console.log('Connection failed.');
-    })
+    connection.once('open', () => {
+        console.log('Database connected 🥳🥳🥳🥳');
+    }).catch(err => {
+        console.log('Connection failed ☹️☹️☹️☹️');
+    });
 }
 
 
